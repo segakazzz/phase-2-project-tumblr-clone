e0a84e938265f8e57e1aab4f60beb5405b08d9ee
Kazue Sasatani <segakazue@gmail.com>
2019-10-25 10:10:12 -0500

    updated knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -32,11 +32,11 @@ module.exports = {
     client: 'pg',
     // The next line is where the application will read that environment variable to connect to the database
     connection: 'postgres://tdumfhpokwwudo:e410a84b53f203be92e58beec14376c0db333b1f19a98bfde4eea0670b3f80ab@ec2-174-129-18-42.compute-1.amazonaws.com:5432/d99kfk37kqqtm6',
-    migrations: {
-      directory: __dirname + '/migrations',
-    },
-    seeds: {
-      directory: __dirname + '/seeds',
-    },
+    // migrations: {
+    //   directory: __dirname + '/migrations',
+    // },
+    // seeds: {
+    //   directory: __dirname + '/seeds',
+    // },
   },
 }
